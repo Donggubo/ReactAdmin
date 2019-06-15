3dee24cc5197de973cd7c6121d55aae9f6d0ae39
dong <806042137@qq.com>
2019-06-15 11:37:38 +0900

    app ~10

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*导入React*/
+import React, {Component} from 'react';
+/*按需导入路由*/
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+/*导入Admin和Login组件*/
+import Admin from '@/pages/admin/admin';
+import Login from '@/pages/login/login';
+export default class App extends Component{
+    render() {
+        return (
+            <div>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path='/login' component={Login}/>
+                        <Route path='/' component={Admin}/>
+                    </Switch>
+                </BrowserRouter>
+            </div>
+        )
+    }
 }
-
-export default App;
---
+++ src/pages/admin/admin.jsx
@@ -0,0 +1,8 @@
+import React, {Component} from "react";
+export default class Admin extends Component {
+    render() {
+        return <div>
+            Admin组件
+        </div>
+    }
+}
